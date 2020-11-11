fd934f9539a2400ec3c5a4e4c3a0325d0167a088
David Hardy <david.hardy50@gmail.com>
2020-11-11 13:50:48 +0100

    fix: error when outside of map on north and south sides

--- helper/index.js
+++ helper/index.js
@@ -1,7 +1,7 @@
 function mazeSolver (maze, userInput) {
   let y = maze.findIndex(arr => arr.includes('F'));
   let x = maze[y].indexOf('F');
-  let position = maze[y][x];
+  let position;
   let doomFound = false;
   const directions = userInput.split(',').map(el=>el.toLowerCase());
   for (let i = 0; i < directions.length; i++) {
@@ -9,8 +9,8 @@ function mazeSolver (maze, userInput) {
     if (directions[i] === 's') y++;
     if (directions[i] === 'w') x--;
     if (directions[i] === 'e') x++;
+    if (y < 0 || x < 0 || x > maze.length-1 || y > maze.length-1 ) return 'Oops, you fell out of the map. You died.';
     position = maze[y][x];
-    if (position === undefined) return 'Oops, you fell out of the map. You died.';
     if (position === 'O') return 'Oops, you found an orc. You died.';
     if (position === 'D' && i === directions.length-1) return 'Well done. You found Mount Doom and destroyed the ring.';
     if (position === 'D') doomFound = true;
